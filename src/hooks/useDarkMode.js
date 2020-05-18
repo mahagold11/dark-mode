44981451ebc3488141d1ba8bcd05501188d2029a
Mark Goldhardt <markgoldhardt@Marks-iMac.hsd1.ut.comcast.net>
2020-05-17 20:22:13 -0600

    mvp

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import useLocalStorage from './useLocalStorage.js';
+
+const useDarkMode = (initialValue) => {
+  const [darkMode, setDarkMode] = useLocalStorage(initialValue);
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }, [darkMode]);
+  return [darkMode, setDarkMode];
+};
+
+export default useDarkMode;
+
+
+
